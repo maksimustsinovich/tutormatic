8f5262a66fcfcae6a382e4ad4d712ea82bb75727
Maksim Ustsinovich <maksimustsinovich@gmail.com>
2025-11-15 12:29:39 +0300

    chore(auth-service): initialize auth service project

---
+++ tutormatic-auth-service/src/main/java/by/ustsinovich/tutormatic/TutormaticAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package by.ustsinovich.tutormatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutormaticAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutormaticAuthServiceApplication.class, args);
+    }
+
+}
