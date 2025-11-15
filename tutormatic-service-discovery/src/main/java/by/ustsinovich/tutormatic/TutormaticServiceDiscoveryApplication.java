4ff7d98c32b6463ef547a03eb592f43a7d6d6689
Maksim Ustsinovich <maksimustsinovich@gmail.com>
2025-11-15 12:24:44 +0300

    chore(service-discovery): initialize service discovery project

---
+++ tutormatic-service-discovery/src/main/java/by/ustsinovich/tutormatic/TutormaticServiceDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package by.ustsinovich.tutormatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutormaticServiceDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutormaticServiceDiscoveryApplication.class, args);
+    }
+
+}
