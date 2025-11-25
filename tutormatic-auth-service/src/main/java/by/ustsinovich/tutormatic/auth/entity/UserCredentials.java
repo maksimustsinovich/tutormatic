f403052aeb84038e5945c664c95398c28ace243e
Maksim Ustsinovich <maksimustsinovich@gmail.com>
2025-11-25 10:09:37 +0300

    feat(auth-service): add user credentials entity

---
+++ tutormatic-auth-service/src/main/java/by/ustsinovich/tutormatic/auth/entity/UserCredentials.java
@@ -0,0 +1,30 @@
+package by.ustsinovich.tutormatic.auth.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "user_credentials", schema = "users")
+public class UserCredentials {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false, updatable = false, unique = true)
+    private UUID id;
+
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+}
