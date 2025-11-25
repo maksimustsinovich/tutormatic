27455d63df273547271214f6b12610fcadf403b9
Maksim Ustsinovich <maksimustsinovich@gmail.com>
2025-11-25 12:40:56 +0300

    feat(auth-service): add Security Config

---
+++ tutormatic-auth-service/src/main/java/by/ustsinovich/tutormatic/auth/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package by.ustsinovich.tutormatic.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/swagger-ui.html",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**"
+                        )
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                ).build();
+    }
+
+}
